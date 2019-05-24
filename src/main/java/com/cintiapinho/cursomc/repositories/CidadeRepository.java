028504572902e03f709f8f31104a97eff584c250
Cintia Pinho <cintia.pinho01@gmail.com>
2019-05-23 21:19:50 -0300

    estado e cidade

---
+++ src/main/java/com/cintiapinho/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,18 @@
+package com.cintiapinho.cursomc.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cintiapinho.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository <Cidade, Integer> {
+
+	Optional<Cidade> findById(Integer id);
+	
+	
+	
+
+}
